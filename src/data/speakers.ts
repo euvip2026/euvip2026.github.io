cb67b64475c124708f814c83ebe7f67352f13883
Eya KHAMASSI <eya.khamassi@unij3m6xc3h6m.uni.lux>
2026-02-09 18:16:36 +0100

    add speaker

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -15,15 +15,19 @@ export const speakers: Speaker[] = [
   // Example:
   {
     name: 'Dima Damen',
-    affiliation: 'Professor & head of research group',
-    description: 'United Kingdom',
+    affiliation: 'University of Bristol & Google DeepMind',
+    // description: 'United Kingdom',
     imageUrl: '/speakers/DimaDamen.jpg',
     profileUrl: 'https://dimadamen.github.io/',
+    talkTitle: 'Modeling in an Ego-Sensed World',
+    talkAbstract:
+      'Forecasting the rise of wearable devices equipped with audio-visual feeds and onboard spatial sensors, this talk will present novel approaches for generative and perception modelling in egocentric videos. The talk argues for new ways to foresee egocentric videos as partial observations of a dynamic 3D world, where objects are out of sight but not out of mind and activities take place in a physically-grounded space. I’ll review new data collection and digital twin annotations in HD-EPIC (https://hd-epic.github.io/) that merges video understanding with 3D modelling, showcasing current failures of VLMs in understanding the perspective outside the camera’s field of view — a task trivial for humans.',
+    bio: 'Dima Damen is a Professor of Computer Vision at the University of Bristol and Senior Research Scientist at Google DeepMind. Dima is currently an EPSRC Fellow (2020-2026), focusing her research interests in the automatic understanding of object interactions, actions and activities using wearable visual (and depth) sensors. She is best known for her leading works in Egocentric Vision, and has also contributed to novel research questions including mono-to-3D, video object segmentation, assessing action completion, domain adaptation, skill/expertise determination from video sequences, discovering task-relevant objects, dual-domain and dual-time learning as well as multi-modal fusion using vision, audio and language.She is the project lead for EPIC-KITCHENS, the seminal dataset in egocentric vision, with accompanying open challenges and follow-up works: EPIC-Sounds, VISOR and EPIC Fields, as well as the recent HD-EPIC. She is part of the large-scale consortium effort Ego4D and Ego-Exo4D. She is an ELLIS Fellow, associate editor (AE) of IJCV, and was a program chair for ICCV 2021 and Associate Editor-in-Chief (AEIC) of IEEE TPAMI (2023-2025). She is frequently a Senior Area Chair and Area Chair in major conferences and was selected as Outstanding SAC in ECCV 2024 and Outstanding Reviewer in CVPR2021, CVPR2020, ICCV2017, CVPR2013 and CVPR2012. Dima received her PhD from the University of Leeds (2009), joined the University of Bristol as a Postdoctoral Researcher (2010-2012), Assistant Professor (2013-2018), Associate Professor (2018-2021) and was appointed as chair in August 2021. She supervises 9 PhD students and 3 Visiting PhD students.',
   },
   {
     name: 'Elisa Ricci',
-    affiliation: 'Professor & head of research unit',
-    description: 'Italy',
+    affiliation: 'University of Trento & Fondazione Bruno Kessler',
+    // description: 'Italy',
     imageUrl: '/speakers/ElisaRicci.jpeg',
     profileUrl: 'https://eliricci.eu/',
     talkTitle: 'Rethinking Multimodal AI Models: Beyond Accuracy, Towards Trust',
@@ -33,9 +37,18 @@ export const speakers: Speaker[] = [
   },
   {
     name: 'Fatma Güney',
-    affiliation: 'Assistant professor',
-    description: 'Turkey',
+    affiliation: 'Koç University',
+    // description: 'Turkey',
     imageUrl: '/speakers/FatmaGüney.jpeg',
     profileUrl: 'https://mysite.ku.edu.tr/fguney/',
   },
+
+  {
+    name: 'Silvia Zuffi',
+    affiliation: 'IMATI-CNR',
+    // description: 'Turkey',
+    imageUrl: '/speakers/SilviaZuffi.jpg',
+    profileUrl: 'https://scholar.google.com/citations?user=-d7Ib5UAAAAJ&hl=en',
+  },
 ]
+
