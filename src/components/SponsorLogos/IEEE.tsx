8cdd8fd6b9d446611e6ddab8b12ebe2cdd4dcce8
Katarina Bozic <katarina.bozic@uni.lu>
2026-05-04 21:54:55 +0200

    wip

--- src/components/SponsorLogos/IEEE.tsx
+++ src/components/SponsorLogos/IEEE.tsx
@@ -1,3 +1,3 @@
 export default function IEEE({ className }: { className?: string }) {
-  return <img className={className} src="/partners/IEEE.png" alt="IEEE Logo" />
+  return <img className={className} src="/partners/ieee.png" alt="IEEE Logo" />
 }
