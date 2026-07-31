7ea9819ffa32b5dd9f9dd569ee7dc6823ee2bb72
Eya KHAMASSI <eya.khamassi@uni.lu>
2026-07-31 18:28:22 +0200

    changes

--- src/app/attend/page.tsx
+++ src/app/attend/page.tsx
@@ -398,7 +398,7 @@ export default function AttendPage() {
                       1
                     </span>
                     <p className="pt-1 leading-relaxed">
-                      Click "Book Your Room" to open the hotel's online booking page.
+                      Click "Book your room" to open the hotel's online booking page.
                     </p>
                   </li>
 
@@ -433,7 +433,7 @@ export default function AttendPage() {
                   rel="noreferrer"
                   className="bg-accent mt-7 inline-flex items-center justify-center px-5 py-3 text-base font-bold text-white shadow-sm transition hover:opacity-90 md:text-lg"
                 >
-                  Book your Room
+                  Book your room
                 </a>
               </div>
 
@@ -494,13 +494,14 @@ export default function AttendPage() {
             {/* Notice */}
             <div className="border-muted mt-10 border-t pt-8">
               <h4 className="text-xl font-extrabold">Additional Information</h4>
-              <p className="mt-5 text-base leading-relaxed text-[#be2222ff] md:text-lg">
+              <p className="mt-2 text-base md:text-lg">Thank you for choosing to have your event with us.</p>
+              
+              <ul className="mt-5 list-disc space-y-3 pl-6 text-base leading-relaxed marker:text-[#2f566e] md:text-lg">
+                <li>
+                <p className="mt-5 text-base leading-relaxed text-[#be2222ff] md:text-lg">
                 Booking at this hotel is only possible within the window of <strong>27/09/2026</strong> to{' '}
                 <strong>01/10/2026</strong>.
               </p>
-              <p className="mt-2 text-base md:text-lg">Thank you for choosing to have your event with us.</p>
-              <ul className="mt-5 list-disc space-y-3 pl-6 text-base leading-relaxed marker:text-[#2f566e] md:text-lg">
-                <li>
                   <p className="mt-2 text-base md:text-lg">
                     For those who wish to stay the night of 01/10 to 02/10, we have arranged another hotel next to the
                     airport (the <strong>preferential</strong> rate for EUVIP attendees is 120 euros, breakfast
